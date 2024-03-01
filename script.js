2ac2cefb986df4137fba27d73eaf4f6457f84ce5
anuraG-ez <160966588+anuraG-ez@users.noreply.github.com>
2024-03-02 03:57:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+// const BASE_URL =
+//   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
 
- const BASE_URL =
+const BASE_URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
+
+
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
+const lastUpdate = document.querySelector("#last-update");
+const exchangeBtn = document.querySelector(".exchange-btn");
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -39,14 +45,17 @@ const updateExchangeRate = async () => {
   try {
     let response = await fetch(URL);
     let data = await response.json();
-
+  
+    let updatedDate = data["date"];
+   
     let fromC =fromCurr.value.toLowerCase() ;
     let toC =toCurr.value.toLowerCase();
- 
+  
     let rate = data[fromC][toC];
 
     let finalAmount = amtVal * rate;
     msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    lastUpdate.innerText = ` Last update: ${updatedDate}`;
   } catch (error) {
     console.error("Error fetching exchange rate:", error);
   }
@@ -64,6 +73,20 @@ btn.addEventListener("click", (evt) => {
   evt.preventDefault();
   updateExchangeRate();
 });
+
+exchangeBtn.addEventListener("click",(evt)=>{
+  let temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+
+  // Update the flag icons
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+
+  // Update the exchange rate
+  updateExchangeRate();
+})
+
 window.addEventListener("load", () => {
     const inputField = document.querySelector(".amount input");
     inputField.focus(); // Focus on the input field
